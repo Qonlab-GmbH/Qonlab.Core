26f5f040c3cce3f40392840fb4adf18eccbb31a2
Sebastian Klatte <sebastian.klatte@trust-communication.com>
2021-04-21 18:06:50 +0200

    Version 1.0.4.

--- Qonlab.Core/DictionaryExtensions.cs
+++ Qonlab.Core/DictionaryExtensions.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace Qonlab.Core {
     public static class DictionaryExtensions {
         [DebuggerStepThrough]
-        public static TValue GetValueOrDefault<TKey, TValue>( this Dictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue ) {
+        public static TValue GetValueOrDefault<TKey, TValue>( this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue ) {
             TValue value;
             return dictionary.TryGetValue( key, out value ) ? value : defaultValue;
         }
